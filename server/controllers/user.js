491f1e0217c8a7c235f41022aed0591fe42918f1
adeelkbw <adeelnasirkbw@gmail.com>
2022-07-09 23:10:33 +0500

    forgot password complete using aws ses --server

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const AWS = require('aws-sdk');
 
 
 exports.register = async (req, res) => {
@@ -116,8 +118,6 @@ exports.logout = async (req, res) => {
 
 
 
-
-
 const sendToken = async (res, statutsCode, user) => {
 
     const token = await user.userToken()
@@ -133,4 +133,87 @@ const sendToken = async (res, statutsCode, user) => {
             success: true,
             data: { name: user.name, email: user.email, _id: user._id, token }
         })
+}
+
+
+exports.forgotPassword = async (req, res) => {
+
+
+    AWS.config.update({
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_ACCESS_SECRET,
+        region: process.env.AWS_REGION
+    });
+
+    const { email } = req.body;
+
+    User.findOne({ email }).exec((err, user) => {
+        if (!user || err) {
+            return res.status(404).json({
+                success: false,
+                data: `No user found with ${email} Email`
+            })
+        }
+
+        const token = jwt.sign({ name: user.name }, process.env.JWT_PRIVATE_KEY, { expiresIn: '20m' })
+        const resetLink = `${process.env.SERVER_URL}/auth/password/reset/${token}`
+
+        // const message = `Your password reset token is: \n\n ${resetLink} \n\n  click to reset your password`;
+
+        user.updateOne({ resetPasswordLink: token }).exec((err, success) => {
+            if (err) {
+                return res.status(404).json({
+                    success: false,
+                    data: "Reset passoward Failed"
+                })
+            }
+
+            var params = {
+                Destination: {
+                    ToAddresses: [
+                        user.email
+                    ]
+                },
+                Message: {
+                    Body: {
+                        Html: {
+                            Charset: "UTF-8",
+                            Data: `<h2>Hey! ${user.name}</h2>
+                            <p>Click the following link to reset your password</p>
+                            <p>${resetLink}</p>
+                            `
+                        },
+                    },
+                    Subject: {
+                        Charset: 'UTF-8',
+                        Data: 'Follow the link to reset your passord'
+                    }
+                },
+                Source: process.env.EMAIL_FROM, /* required */
+                ReplyToAddresses: [
+                    process.env.EMAIL_TO
+                ],
+            };
+
+            const sendPromise = new AWS.SES({ apiVersion: '2010-12-01' }).sendEmail(params).promise();
+            sendPromise.then((data) => {
+                console.log(data);
+                res.json({
+                    Success: true,
+                    token: token,
+                    data: `Email has been sent to ${user.email}`
+                })
+            }).catch(err => {
+                console.log(err);
+                res.status(400).json({
+                    success: false,
+                    data: "Email sent failed try again with a Valid email"
+                })
+
+            })
+
+
+        })
+
+    })
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { register, login, logout } = require('../controllers/user')
+const { register, login, logout, forgotPassword } = require('../controllers/user')
 
 router.post('/register', register)
 router.post('/login', login)
 router.get('/logout', logout)
+router.post('/password/reset', forgotPassword)
 
 module.exports = router
